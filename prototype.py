2160ea5952528d455ad854f31d0714f9babfee44
SabeehaMalikah <sabeeha.malikah@gmail.com>
2026-09-14 00:52:59 -0400

    fixed formatting error

--- prototype.py
+++ prototype.py
@@ -56,10 +56,46 @@ masjid6 = {
     "womens_section" : False,
     "denomination" : "Shia",
     "open" : "0:00",
-    "close" : "0:00",
+    "close" : "23:59",
     "rating" : 4.8
 }
-masjids = [masjid1, masjid2, masjid3, masjid4, masjid5, masjid6]
+masjid7 = {
+    "name" : "Islamic Cultural Center of New York",
+    "address" : "1711 3rd Ave, New York, NY 10029",
+    "womens_section" : True,
+    "denomination" : "Sunni",
+    "open" : "10:00",
+    "close" : "17:00",
+    "rating" : 4.7
+}
+masjid8 = {
+    "name" : "Masjid Manhattan",
+    "address" : "30 Cliff St, New York, NY 10038",
+    "womens_section" : True,
+    "denomination" : "Sunni",
+    "open" : "4:00",
+    "close" : "22:30",
+    "rating" : 4.8
+}
+masjid9 = {
+    "name" : "An-Noor Masjid",
+    "address" : "70-13 37 Avenue,(Basement, Jackson Heights, NY 11372",
+    "womens_section" : False,
+    "denomination" : "Sunni",
+    "open" : None,
+    "close" : None,
+    "rating" : 4.4
+}
+masjid10 = {
+    "name" : "MAS Staten Island Center (MASSI Center)",
+    "address" : "180 Burgher Ave, Staten Island, NY 10304",
+    "womens_section" : True,
+    "denomination" : "Sunni",
+    "open" : "6:00",
+    "close" : "22:30",
+    "rating" : 4.8
+}
+masjids = [masjid1, masjid2, masjid3, masjid4, masjid5, masjid6, masjid7, masjid8, masjid9, masjid10]
 
 def has_womens_section(masjids, masjid_name):
     for masjid in masjids:
@@ -86,10 +122,17 @@ def filter_open(masjids):
 
 def filter_borough(masjids, target_borough):
     result = [masjid for masjid in masjids if target_borough in masjid["address"]]
+    if target_borough == "Queens:":
+        result = []
+        for masjid in masjids:
+            if masjid["address"][:-5][:3] == "110" or masjid["address"][:-5][:3] == "111" or \
+                masjid["address"][:-5][:3] == "113" or masjid["address"][:-5][:3] == "114" or \
+                masjid["address"][:-5][:3] == "116":
+                result.append(masjid)
     return result
 
 def print_directory(masjids_list=masjids):
-    i = 0
+    i = 1
     for masjid in masjids_list:
         print(
             f"{i}. {masjid["name"]}\n" \
@@ -105,41 +148,71 @@ def main():
         "Please see the list of masaajid below:\n"
     )
     print_directory()
-    
-    while True:
-        filter_option = input(
+
+    # prompt the user
+    filter_options = input(
             "\nPlease choose from the following filtering options: \n" \
             "'W' -- for masaajid with womens' prayer area \n" \
             "'SN' -- for Sunni masaajid\n" \
             "'SH' -- for Shia masaajid\n" \
             "'B' -- for masaajid in Brooklyn\n" \
-            "'Q' -- for masaajid in Queens\n" \
             "'M' -- for masaajid in Manhattan\n" \
+            "'Q' -- for masaajid in Queens\n" \
+            "'BR' for masaajid in the Bronx\n" \
+            "'SI' -- for masaajid in Staten Island\n" \
+            "'X' -- to exit\n\n" \
+            "Enter your response as a list of all the filters you would like to apply.\n" \
+            "If you would like to see sunni masjids in Brooklyn, you would enter 'SN B'.\n"
+        )
+    valid_filters = ["W", "SN", "SH", "B", "M", "Q", "BR", "SI", "X"]
+    filter_options = filter_options.split()
+    valid = True
+    for f in filter_options:
+        if f.upper() not in valid_filters:
+            valid = False
+    if not valid:
+        filter = input(
+            "\nInvalid filter entered. \nPlease choose from the following filtering options: \n" \
+            "'W' -- for masaajid with womens' prayer area \n" \
+            "'SN' -- for Sunni masaajid\n" \
+            "'SH' -- for Shia masaajid\n" \
+            "'B' -- for masaajid in Brooklyn\n" \
+            "'M' -- for masaajid in Manhattan\n" \
+            "'Q' -- for masaajid in Queens\n" \
             "'BR' for masaajid in the Bronx\n" \
             "'SI' -- for masaajid in Staten Island\n" \
             "'X' -- to exit the menu\n\n"
         )
-        if filter_option.upper() == 'W':
-            print_directory(filter_womens_section(masjids))
-        elif filter_option.upper() == 'SN':
-            print_directory(filter_sunni(masjids))
-        elif filter_option.upper() == 'SH':
-            print_directory(filter_shia(masjids))
-        elif filter_option.upper() == 'B':
-            print_directory(filter_borough(masjids, "Brooklyn"))
-        elif filter_option.upper() == 'Q':
-            print_directory(filter_borough(masjids, "Queens"))
-        elif filter_option.upper() == 'M':
-            print_directory(filter_borough(masjids, "New York"))
-        elif filter_option.upper() == 'BR':
-            print_directory(filter_borough(masjids, "Bronx"))
-        elif filter_option.upper() == 'SI':
-            print_directory(filter_borough(masjids, "Staten Island"))
-        elif filter_option.upper() == 'X':
+    result = masjids
+    for filter in filter_options:
+        if filter.upper() == 'W':
+            result = filter_womens_section(result)
+        elif filter.upper() == 'SN':
+            result = filter_sunni(result)
+        elif filter.upper() == 'SH':
+            result = filter_shia(result)
+        elif filter.upper() == 'B':
+            result = filter_borough(result, "Brooklyn")
+        elif filter.upper() == 'Q':
+            result = filter_borough(result, "Queens")
+        elif filter.upper() == 'M':
+            result = filter_borough(result, "New York")
+        elif filter.upper() == 'BR':
+            result = filter_borough(result, "Bronx")
+        elif filter.upper() == 'SI':
+            result = filter_borough(result, "Staten Island")
+        elif filter.upper() == 'X':
             print("Thank you for choosing Masjid Navigator!")
             exit()
         else:
             print("Please choose from the given filter options.")
+    if result == []:
+        print("Sorry, no masjids found.")
+
+    print("\n\nFiltered Masjids:")
+    print_directory(result)
+    print("Thank you for choosing Masjid Navigator!")
+
 
 main()
 
